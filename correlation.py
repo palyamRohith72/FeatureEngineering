e057ed32817fde8c8a413695407b85ddeb6fad43
palyamRohith72 <palyamrohith72@gmail.com>
2025-02-24 07:53:50 +0530

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -13,22 +13,17 @@ class Correlation:
         
         with tab1:
             col1, col2 = st.columns([1, 2],border=True)
-            
-            with col1:
-                method = st.radio("Select Correlation Method", ["pearson", "kendall", "spearman"], index=0)
-                numeric_only = st.checkbox("Include only numeric data", value=False)
-            
-            with col2:
-                st.subheader("Correlation Matrix", divider='blue')
-                numeric_only = st.checkbox("Include only numeric data", value=False)
-                if st.button("Compute Correlation",use_container_width=True,type='primary'):
-                    corr_matrix = self.dataset.corr(method=method, numeric_only=numeric_only)
-                    st.dataframe(corr_matrix)
-                    
-                    # Plot heatmap
-                    fig, ax = plt.subplots()
-                    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f', ax=ax)
-                    st.pyplot(fig)
+            method = col1.radio("Select Correlation Method", ["pearson", "kendall", "spearman"], index=0)
+            col2.subheader("Correlation Matrix", divider='blue')
+            numeric_only = col2.checkbox("Include only numeric data", value=False)
+            if col2.button("Compute Correlation",use_container_width=True,type='primary'):
+                corr_matrix = self.dataset.corr(method=method, numeric_only=numeric_only)
+                col2.dataframe(corr_matrix)
+                
+                # Plot heatmap
+                fig, ax = plt.subplots()
+                sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f', ax=ax)
+                col2.pyplot(fig)
         
         with tab2:
             st.subheader("Dataset Preview", divider='blue')
