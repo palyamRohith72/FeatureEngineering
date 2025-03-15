686cd0f2e1b10a00b1cf2a41187d7543639c54c8
palyamRohith72 <palyamrohith72@gmail.com>
2025-03-15 18:56:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ class Features:
             "Drop Duplicated Features": drop_duplicated_features,
             "Drop Correlated Features": drop_correlated_features,
             "Smart Correlated Selection": smart_correlated_selection,
-            "MRMR": self.mrmr,
+            "MRMR": mrmr,
             "Select By Single Feature Performance": select_by_single_feature_performance,
             "Recursive Feature Elimination": recursive_feature_elimination,
             "Recursive Feature Addition": recursive_feature_addition,
