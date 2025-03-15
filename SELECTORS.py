82faa14bdac628a560b04b7158f33f667966683c
palyamRohith72 <palyamrohith72@gmail.com>
2025-03-15 19:05:21 +0530

    Update SELECTORS.py

--- SELECTORS.py
+++ SELECTORS.py
@@ -4,7 +4,7 @@ import streamlit as st
 import numpy as np
 from feature_engine.selection import *
 def drop_features(keyy,data):
-  select_columns = st.multiselect("Select columns", self.dataset.columns.tolist())
+  select_columns = st.multiselect("Select columns", data.columns.tolist())
   if select_columns:
       dataset = data.copy(deep=True)
       if st.button("Execute Feature Selection", use_container_width=True):   
@@ -13,7 +13,7 @@ def drop_features(keyy,data):
           st.session_state[keyy]=dataframe
           st.dataframe(dataframe)
 def drop_constant_features(option,data):
-    select_columns = st.multiselect("Select columns", self.dataset.columns.tolist())
+    select_columns = st.multiselect("Select columns", data.columns.tolist())
     tol=int(st.number_input("""Threshold to detect constant/quasi-constant features. Variables showing the same value in a
     percentage of observations greater than tol will be considered 
     constant / quasi-constant and dropped. If tol=1, the transformer 
@@ -30,7 +30,7 @@ def drop_constant_features(option,data):
         except Exception as e:
             st.error(e)
 def drop_duplicated_features(option,data):
-    select_columns = st.multiselect("Select columns", self.dataset.columns.tolist())
+    select_columns = st.multiselect("Select columns", data.columns.tolist())
     dataset = data.copy(deep=True)
     if st.button("Execute Feature Selection", use_container_width=True):   
         try:
@@ -42,7 +42,7 @@ def drop_duplicated_features(option,data):
             st.error(e)
 
 def drop_correlated_features(option,data):
-    select_columns = st.multiselect("Select columns", self.dataset.columns.tolist())
+    select_columns = st.multiselect("Select columns", data.columns.tolist())
     threshold=st.number_input("The correlation threshold above which a feature will be deemed correlated with another one and removed from the dataset.",0.1)
     method=st.selectbox("Correlation method -Can take ‘pearson’, ‘spearman’, ‘kendall’",["pearson","spearman","kendall"])
     dataset = data.copy(deep=True)
